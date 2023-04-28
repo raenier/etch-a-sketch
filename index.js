0da514e57f2ebd8d6f09bd6622aeb374cc48a8df
raenier <raenierquiambao@gmail.com>
2023-04-28 09:46:59 +0800

    Enable user to adjust resolution

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const container = document.querySelector('.sketch-board');
-let boxeshtml = '';
-for(let index = 0; index < 64 * 64; index++) {
-  boxeshtml += " <div class='box'></div>";
-}
-container.innerHTML = boxeshtml;
+const reso_btn = document.querySelector('.reso_btn');
 
-const boxes = document.querySelectorAll('.box');
-for(box of boxes) {
-  box.addEventListener('mouseover', (e) => {
-    e.target.style.background = 'black';
-  });
+//Function for creating the boxes
+function createBoxes(boxmultiplier, container) {
+  let boxeshtml = '';
+  for(let index = 0; index < boxmultiplier * boxmultiplier; index++) {
+    boxeshtml += " <div class='box'></div>";
+  }
+  container.innerHTML = boxeshtml;
+  
+  const boxes = document.querySelectorAll('.box');
+  for(box of boxes) {
+    box.addEventListener('mouseover', (e) => {
+      e.target.style.background = 'black';
+    });
+    box.style.flexBasis = `calc(100% / ${boxmultiplier})`;
+    box.style.paddingBottom = `calc(100% / ${boxmultiplier})`;
+  }
 }
+
+//Create initial boxes 100 by 100
+createBoxes(100, container);
+
+//Determine resolution/ get from user
+reso_btn.addEventListener('click', (e) => {
+  let boxmultiplier = prompt('Range of 1-100');
+  createBoxes(boxmultiplier, container);
+});
